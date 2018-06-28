57893a3f07be2dd000a9dbd1f13bb902a7ac0af5
Marcus Venghaus <info@inklammern.de>
2018-06-28 10:57:05 +0200

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Magento 2 - Fix for Amazon Pay with Mageplaza OSC
+Tested with Mageplaza_Osc 1.5.0 and Amazon_Payment 2.0.12 
---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'Amazon_Payment/js/view/shipping': 'Inkl_MageplazaOscAmazonPay/js/view/shipping',
+            'Amazon_Payment/js/view/form/element/email': 'Inkl_MageplazaOscAmazonPay/js/view/form/element/email'
+        }
+    }
+};
